a77f37b66875eaafb70b749e67629bf31adb4389
Sangmin <101931428+netco97@users.noreply.github.com>
2023-06-30 20:31:09 +0900

    회원가입 이메일 중복처리 (ajax)

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -93,4 +93,17 @@ public class MemberController {
 
         return "index";
     }
+
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail){
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
+
+    //    if(checkResult != null){
+    //    return "ok";
+    //    }else{
+    //        return "no";
+    //    }
+    }
 }
--- src/main/java/com/example/member/service/MemberService.java
+++ src/main/java/com/example/member/service/MemberService.java
@@ -89,4 +89,15 @@ public class MemberService {
         //deleteById메서드만 쓰면 됨.
         memberRepository.deleteById(id);
     }
+
+    public String emailCheck(String memberEmail) {
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+        if (byMemberEmail.isPresent()){
+            // 조회결과가 있다 -> 사용할 수 없다.
+            return null;
+        }else{
+            // 조회결과가 없다 -> 사용할 수 있다.
+            return "ok";
+        }
+    }
 }
